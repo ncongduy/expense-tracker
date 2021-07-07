956e2d1cda0ed6183a3d3cd1164597790764a341
Nguyen Cong Duy <ncongduy@gmail.com>
2021-07-07 16:07:30 +0300

    build expense tracker by using @reduxjs/toolkit

---
+++ src/features/budgets/budgetsSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const CATEGORIES = [
+  "housing",
+  "food",
+  "transportation",
+  "utilities",
+  "clothing",
+  "healthcare",
+  "personal",
+  "education",
+  "entertainment",
+];
+const initialState = CATEGORIES.map((category) => ({
+  category: category,
+  amount: 0,
+}));
+
+const budgetsSlice = createSlice({
+  name: "budgets",
+  initialState: initialState,
+  reducers: {
+    editBudget: (state, action) => {
+      const newBudgets = state.map((budget) => {
+        if (budget.category === action.payload.category) {
+          return action.payload;
+        }
+        return budget;
+      });
+      return newBudgets;
+    },
+  },
+});
+
+export const { editBudget } = budgetsSlice.actions;
+
+export default budgetsSlice.reducer;
+
+export const selectBudgets = (state) => state.budgets;
